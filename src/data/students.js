0130a6e61f43980dc6a51697c757c540b2fd2f27
hobbit12b <hobbit12b@gmail.com>
2026-06-25 23:29:20 +0200

    Reset stored starter players once

--- src/data/students.js
+++ src/data/students.js
@@ -1,2 +1,15 @@
-const STUDENTS = [];
 const OFFERED_LETTERS_2025_2026 = ["w", "r", "t", "p", "a", "s", "g", "h", "k", "l", "z", "v", "b", "n", "m"];
+
+try {
+  const versionKey = "woordjestovenaar.contentVersion";
+  const version = "2025-2026-empty-start";
+  if (localStorage.getItem(versionKey) !== version) {
+    localStorage.setItem("woordjestovenaar.players", "[]");
+    localStorage.setItem("woordjestovenaar.deletedPlayers", "[]");
+    localStorage.setItem(versionKey, version);
+  }
+} catch (error) {
+  // Opslag is niet noodzakelijk om de basis te gebruiken.
+}
+
+const STUDENTS = [];
