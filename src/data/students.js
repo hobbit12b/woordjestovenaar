661f55f7391183a2c74ceca6438cd996af633f14
hobbit12b <hobbit12b@gmail.com>
2026-06-25 23:25:59 +0200

    Clear starter students and add offered letters

--- src/data/students.js
+++ src/data/students.js
@@ -1,5 +1,2 @@
-const STUDENTS = [
-  { id: "sem", name: "Sem", knownLetters: ["b", "i", "g", "k", "a", "t", "m", "o", "l"] },
-  { id: "noor", name: "Noor", knownLetters: ["j", "a", "s", "b", "u", "s", "p", "o"] },
-  { id: "test", name: "Test", knownLetters: ["a","b","d","e","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","w","z"] }
-];
+const STUDENTS = [];
+const OFFERED_LETTERS_2025_2026 = ["w", "r", "t", "p", "a", "s", "g", "h", "k", "l", "z", "v", "b", "n", "m"];
